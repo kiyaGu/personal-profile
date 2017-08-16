3e185ecc99fd9e27d9c6c78a0529c325048a5d1d
KiyaG <hamilee.kiya@gmail.com>
2017-08-16 18:11:58 +0100

    changed the server file to serve a static html

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static("my_portfolio_page"));
+app.use(express.static(__dirname + '/'));
 app.listen(process.env.PORT || 3333, function() {
     console.log('Server is listening on port 3333. Ready to accept requests!');
 });
