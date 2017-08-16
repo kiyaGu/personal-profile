192b73c38a1cc1806a78f4d419518d5d63bf210f
KiyaG <hamilee.kiya@gmail.com>
2017-08-16 11:12:43 +0100

    deploying my node app to heroku

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
-app.listen(3333, function() {
+
+app.use(express.static("my_portfolio_page"));
+app.listen(process.env.PORT || 3333, function() {
     console.log('Server is listening on port 3333. Ready to accept requests!');
 });
