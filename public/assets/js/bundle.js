49dc2716e9724ee63443a79ff27957c5a78abe6c
KiyaG <hamilee.kiya@gmail.com>
2017-09-09 23:59:01 +0100

    modified the tweenmax animation for the home section

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -382,29 +382,10 @@ $(document).ready(function() {
 
     TweenMax.staggerFrom('#move-down a', 8, { x: -100, opacity: 0, rotation: 360, scale: .2, delay: 38, ease: Back.easeIn });
     TweenMax.staggerFrom('#move-down i', 10, { opacity: 0, rotation: 360, scale: .5, delay: 40 });
-    TweenMax.staggerFrom('.hire-me button', 14, { y: 0, opacity: 0, delay: 29, rotation: 90, scale: 0.1, ease: Back.easeIn });
-    TweenMax.staggerFrom('#hire-me-top-hr', 14, { y: -140, opacity: 0, delay: 25, scale: 0.2, ease: Back.easeOut });
-    TweenMax.staggerFrom('#hire-me-bottom-hr', 14, { y: 1000, opacity: 0, delay: 25, scale: 0.2, ease: Back.easeOut });
-    //set up ScrollMagic
-    // var controller = new ScrollMagic.Controller({
-    //     globalSceneOptions: {
-    //         triggerHook: "onLeave"
-    //     }
-    // });
-    // //atractions animation
-    // var attractionstween = TweenMax.staggerFromTo('#about-me', 1, { opacity: 0, scale: 0 }, {
-    //     delay: 1,
-    //     opacity: 1,
-    //     scale: 1,
-    //     ease: Back.easeOut
-    // });
+    TweenMax.staggerFrom('.hire-me button', 14, { y: 0, opacity: 0, delay: 23, rotation: 90, scale: 0.1, ease: Back.easeIn });
+    TweenMax.staggerFrom('#hire-me-top-hr', 14, { y: 0, opacity: 0, delay: 37, scale: 0.2, ease: Back.easeOut });
+    TweenMax.staggerFrom('#hire-me-bottom-hr', 14, { y: 0, opacity: 0, delay: 37, scale: 0.2, ease: Back.easeOut });
 
-    // var topoffset = 43;
-    // var scene = new ScrollMagic.Scene({
-    //         triggerElement: '#about-me-section',
-    //         offset: -topoffset
-    //     }).setTween(attractionstween)
-    //     .addTo(controller);
     //to change the active navigation when the user scrolls to different sections
     changeActiveTopNavigation();
     //animate the progressbar/skills bar when the usesr scrolls
